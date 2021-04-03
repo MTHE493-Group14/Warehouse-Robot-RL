c6b9a6afdc80410971c29e6fb8abf9b89940cfc3
ianho8 <16imh4@queensu.ca>
2021-04-03 12:37:03 -0400

    added epsilon_min_visits_greedy_policy, added the performance object as an attribute, and added numpy functionality to speed things up

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 import copy
 import random
+import numpy as np
 
 from actions import Actions
 from tables import Tables
+from performance import Performance
 from warehouse_parameters import N_ROBOTS
 
 class Agent:
@@ -13,22 +15,32 @@ class Agent:
     ----------
     tables : Tables
         The tables used for training.
+    performance : Performance
+        The dataframe tracking the performance of the greedy policy.
     """
     
-    def __init__(self):
+    def __init__(self, filename, overwrite):
         """
         Creates an Agent object.
         
         The Agent chooses the actions to take according to some policy. It
         also keeps track of q-value estimates, visits, and which actions do 
         not change the robot/stack locations.
+        
+        Parameters
+        ----------
+        filename : str
+            The filename that should be used to read/write the dataframe.
+        overwrite : bool
+            Whether or not the csvs should be overwritten by ones.
 
         Returns
         -------
         None.
 
         """
-        self.tables = Tables()
+        self.tables = Tables(filename, overwrite)
+        self.performance = Performance('tabular', filename, overwrite)
         return
     
     def min_visits_policy(self, current_state):
@@ -47,8 +59,7 @@ class Agent:
 
         """
         snum = current_state.enum()
-        min_visits = min(self.tables.visits[snum])
-        anum = list(self.tables.visits[snum]).index(min_visits)
+        anum = np.argmin(self.tables.visits[snum])
         a = Actions()
         a.set_by_enum(anum)
         return a
@@ -69,12 +80,11 @@ class Agent:
 
         """
         snum = current_state.enum()
-        min_qval = min(self.tables.qvals[snum])
-        anum = list(self.tables.qvals[snum]).index(min_qval)
+        anum = np.argmin(self.tables.qvals[snum])
         a = Actions()
         a.set_by_enum(anum)
         return a
-    
+
     def random_policy(self, current_state):
         """
         Randomly determines an action to take given the current state.
@@ -88,7 +98,7 @@ class Agent:
         a = Actions()
         return a
     
-    def epsilon_greedy_policy(self, current_state, epsilon):
+    def epsilon_random_greedy_policy(self, current_state, epsilon):
         """
         Act randomly with probability epsilon. Otherwise act greedily.
 
@@ -107,6 +117,28 @@ class Agent:
         """
         if random.random() < epsilon:
             return self.random_policy(current_state)
+        else:
+            return self.greedy_policy(current_state)
+
+    def epsilon_min_visits_greedy_policy(self, current_state, epsilon):
+        """
+        Choose least visited action with probability epsilon. Otherwise act greedily.
+
+        Parameters
+        ----------
+        current_state : State
+            The current state the agent is in.
+        epsilon : float
+            The probability that the agent should choose the least visited action.
+
+        Returns
+        -------
+        Actions
+            The actions that should be taken if following this policy.
+
+        """
+        if random.random() < epsilon:
+            return self.min_visits_policy(current_state)
         else:
             return self.greedy_policy(current_state)
         
@@ -209,7 +241,7 @@ class Agent:
                 desired_stack_loc = current_state.stack_locs[desired_stack_idx]
             if max_order == 0 or desired_stack_loc.col == 0:
                 # case 1
-                a.set_all_actions('O')
+                a.set_all_actions('N')
             else:
                 robot_cols = [loc.col for loc in current_state.robot_locs]
                 desired_robot_idx = robot_cols.index(desired_stack_loc.col)
@@ -221,7 +253,7 @@ class Agent:
                         a.set_all_actions('D')
                 else:
                     # case 3
-                    a.set_all_actions('O')
+                    a.set_all_actions('N')
                     if desired_stack_loc.row == 1:
                         a.set_action(desired_robot_idx, 'SU')
                     elif desired_stack_loc.row == 2:
@@ -233,7 +265,7 @@ class Agent:
             desired_robot_loc = current_state.robot_locs[desired_robot_idx]
             if desired_robot_loc.col > 0:
                 # case 4
-                a.set_all_actions('O')
+                a.set_all_actions('N')
                 a.set_action(desired_robot_idx, 'SL')
             else:
                 # case 5
@@ -246,7 +278,7 @@ class Agent:
                     if robot_loc.col < missing_col:
                         a.set_action(robot_idx, 'SR')
                     else:
-                        a.set_action(robot_idx, 'O')
+                        a.set_action(robot_idx, 'N')
                 if desired_robot_loc.row == 0:
                     a.set_action(desired_robot_idx, 'SD')
                 else:
