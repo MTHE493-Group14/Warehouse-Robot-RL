0218478b49eb1d2c14a4f74c527bd4c10b4f9c8d
ianho8 <16imh4@queensu.ca>
2021-04-03 12:12:28 -0400

    added functionality to train/evaluate for function approximation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from environment import Environment
+import time
+import numpy as np
 
 
 """
@@ -32,95 +34,113 @@ states (e.g. if a robot is in the top row of the grid, they may not move up).
 """
 
 
-def train(n_reps=1000, n_iter=30, overwrite=False):
-    env = Environment()
-    if not overwrite:
-        env.agent.tables.read_tables()
+def train(method, train_reps=1000, train_iters=1000, eval_reps=100, eval_iters=100, overwrite=False):
+    env = Environment(method, overwrite)
+    greedy_actions = env.agent.tables.greedy_actions()
+    if overwrite:
+        avg_cost, max_avg_cost, min_avg_cost = evaluate(method, eval_reps, eval_iters)
+        env.agent.performance.update(0, avg_cost, max_avg_cost, min_avg_cost)
         
-    for rep in range(n_reps):
+    for _ in range(train_reps):
         env.state.reset()
-        for time_step in range(n_iter):
-            a = env.agent.min_visits_policy(env.state)
+        for _ in range(train_iters):
+            a = env.agent.epsilon_random_greedy_policy(env.state, 0.95)
+            # a = env.agent.epsilon_random_greedy_policy(env.state, 0.95)
             previous_state = env.state
             env.state = env.calculate_state(env.state, a)
             env.update_cost()
-            env.agent.tables.update(previous_state, env.state, a, sum(env.state.orders))
-    score = evaluate(1000, 50, train=True)
-    env.agent.tables.performance_update(n_reps*n_iter + 50000, score)
-    env.agent.tables.save_tables()
-    
-    return
-
-def evaluate(n_reps=1000, n_iter=50, show=29, train=True):
-    env = Environment()
-    env.agent.tables.read_tables()
-    
-    for time_step in range(show):
-        print('\n')
-        print("t = " + str(time_step))
-        print(env)
-        a = env.agent.greedy_policy(env.state)
-        print("actions = " + str(a.actions))
-        print('state = ' + str(env.state.enum()) + ', action = ' + str(a.enum()))
-        if train:
-            previous_state = env.state
-            env.state = env.calculate_state(env.state, a)
-            env.update_cost()
-            env.agent.tables.update(previous_state, env.state, a, sum(env.state.orders))
-        else:
-            env.state = env.calculate_state(env.state, a)
-            env.update_cost()
-    
-    env.cost = 0
-    for rep in range(n_reps):
-        env.state.reset()
-        for time_step in range(n_iter):
-            a = env.agent.greedy_policy(env.state)
-            if train:
-                previous_state = env.state
-                env.state = env.calculate_state(env.state, a)
-                env.update_cost()
+            if method == 'tabular':
                 env.agent.tables.update(previous_state, env.state, a, sum(env.state.orders))
             else:
-                env.state = env.calculate_state(env.state, a)
-                env.update_cost()
+                env.agent.lfa.update(previous_state, env.state, a, sum(env.state.orders))
 
-    if train:
-        env.agent.tables.save_tables()
-    score = env.cost/n_iter/n_reps
-    print('\nscore = ' + str(score))
-    return score
+    if method == 'tabular':
+        env.agent.tables.save(env.filename)
+    else:
+        env.agent.lfa.save(env.filename)
+
+    avg_cost, max_avg_cost, min_avg_cost = evaluate(method, eval_reps, eval_iters)
+    env.agent.performance.update(train_reps*train_iters, avg_cost, max_avg_cost, min_avg_cost)
+    env.agent.performance.save(method, env.filename)
+    # env.agent.performance.plot()
+    return np.all(greedy_actions == env.agent.tables.greedy_actions())
 
 
 
-def baseline(n_iter=1000, show=10):
-    env = Environment()
-    if not env.state.baseline_organization():
-        return
+
+def evaluate(method, n_reps=100, n_iter=100, show=100):
+    env = Environment(method, overwrite=False)
     
     for time_step in range(show):
+        a = env.agent.greedy_policy(env.state)
         print('\n')
         print("t = " + str(time_step))
-        print(env)
-        a = env.agent.baseline_policy(env.state)
+        print(env.state.grid())
         print("actions = " + str(a.actions))
-        print('state = ' + str(env.state.enum()) + ', action = ' + str(a.enum()))
-        env.state = env.calculate_state(env.state, a)
-        env.update_cost()
-        
-    for time_step in range(n_iter):
-        a = env.agent.greedy_policy(env.state)
+        # print('state = ' + str(env.state.enum()) + ', action = ' + str(a.enum()))
         env.state = env.calculate_state(env.state, a)
         env.update_cost()
     
-    print('\nscore = ' + str(env.cost/n_iter))
-    return
+    env.cost = 0
+    max_cost = 0
+    min_cost = -1
+    for _ in range(n_reps):
+        env.state.reset()
+        start_cost = env.cost
+        for _ in range(n_iter):
+            a = env.agent.greedy_policy(env.state)
+            env.state = env.calculate_state(env.state, a)
+            env.update_cost()
+        rep_cost = env.cost - start_cost
+        max_cost = max(max_cost, rep_cost)
+        if rep_cost < min_cost or min_cost == -1:
+            min_cost = rep_cost
 
-# train(overwrite=True)
-for i in range(10):
-    print('\n', i)
-    train()
+    avg_cost = env.cost/n_iter/n_reps
+    max_avg_cost = max_cost / n_iter
+    min_avg_cost = min_cost / n_iter
+    print(  '\navg_cost = ' + str(avg_cost) + 
+            ', max_avg_cost = ' + str(max_avg_cost) + 
+            ', min_avg_cost = ' + str(min_avg_cost))
+    return avg_cost, max_avg_cost, min_avg_cost
+
+
+
+
+
+
+# def baseline(n_iter=1000, show=10):
+#     env = Environment()
+#     if not env.state.baseline_organization():
+#         return
     
+#     for time_step in range(show):
+#         print('\n')
+#         print("t = " + str(time_step))
+#         print(env)
+#         a = env.agent.baseline_policy(env.state)
+#         print("actions = " + str(a.actions))
+#         print('state = ' + str(env.state.enum()) + ', action = ' + str(a.enum()))
+#         env.state = env.calculate_state(env.state, a)
+#         env.update_cost()
+        
+#     for time_step in range(n_iter):
+#         a = env.agent.greedy_policy(env.state)
+#         env.state = env.calculate_state(env.state, a)
+#         env.update_cost()
+    
+#     print('\nscore = ' + str(env.cost/n_iter))
+#     return
+
+
+# train('fa', train_reps=1000, train_iters=100, eval_reps=100, eval_iters=100, overwrite=True)
+for i in range(1000):
+    print('\n', i)
+    converged = train('tabular', train_reps=1000, train_iters=100, eval_reps=100, eval_iters=100, overwrite=False)
+    if converged:
+        print("LFGGG\n\n\n")
+        break
+
 # evaluate(train=True)
     
 # baseline()
